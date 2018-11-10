dc028f35df4f6cef9fb0b9e4ab1823c47e52bd06
Rohan Rayarikar <rayarikar.rohan@gmail.com>
2018-11-10 16:16:00 -0500

    Adding security around the endpoints

---
+++ src/main/java/com/rohan/development/contractdrivendevelopmentsample/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.rohan.development.contractdrivendevelopmentsample.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SecurityConfig.class);
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception{
+        LOGGER.info("All endpoints secured with no authentication required.");
+        httpSecurity.authorizeRequests().antMatchers("/*").permitAll();
+    }
+}
---
+++ src/main/java/com/rohan/development/contractdrivendevelopmentsample/endpoints/HelloWorldResource.java
@@ -0,0 +1,7 @@
+package com.rohan.development.contractdrivendevelopmentsample.endpoints;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldResource {
+}
