9b95ab25243cc5d9919632c90ca376494484ebca
Rohan Rayarikar <rayarikar.rohan@gmail.com>
2018-11-10 18:53:42 -0500

    Adding end to end swagger config

--- src/main/java/com/rohan/development/contractdrivendevelopmentsample/endpoints/HelloWorldResource.java
+++ src/main/java/com/rohan/development/contractdrivendevelopmentsample/endpoints/HelloWorldResource.java
@@ -1,12 +1,18 @@
 package com.rohan.development.contractdrivendevelopmentsample.endpoints;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/sample")
+@Api(value = "Hello World Endpoints", description = "Sample endpoints for hello world")
 public class HelloWorldResource {
 
-    @RequestMapping("/")
+    @GetMapping(value = "/")
+    @ApiOperation(value = "Gets a sample String response", response = String.class)
     public String helloWorld() {
         return "Hello world from SpringBoot";
     }
