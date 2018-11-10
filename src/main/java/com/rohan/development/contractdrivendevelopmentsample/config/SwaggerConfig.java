5e31d1c28274ad4cc9b783f87f7d3aa17492dfc4
Rohan Rayarikar <rayarikar.rohan@gmail.com>
2018-11-10 18:42:26 -0500

    Adding API meta data

--- src/main/java/com/rohan/development/contractdrivendevelopmentsample/config/SwaggerConfig.java
+++ src/main/java/com/rohan/development/contractdrivendevelopmentsample/config/SwaggerConfig.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
@@ -18,6 +22,19 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(getSwaggerAPIMetaData());
+    }
+
+    private ApiInfo getSwaggerAPIMetaData() {
+        ApiInfo apiInfo = new ApiInfo(
+                "Contract Driven Development Sample",
+                "Spring Boot REST API for Contract Driven Development",
+                "1.0",
+                "Terms of service",
+                new Contact("Rohan Rayarikar", "", ""),
+                "MIT License",
+                "https://opensource.org/licenses/MIT", Collections.emptyList());
+        return apiInfo;
     }
 }
