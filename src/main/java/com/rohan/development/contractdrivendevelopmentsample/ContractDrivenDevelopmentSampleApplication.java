c68e572815d0e2f65864af5864c77b79b8751c53
Rohan Rayarikar <rayarikar.rohan@gmail.com>
2018-11-10 15:35:38 -0500

    Initial commit for the basics

---
+++ src/main/java/com/rohan/development/contractdrivendevelopmentsample/ContractDrivenDevelopmentSampleApplication.java
@@ -0,0 +1,12 @@
+package com.rohan.development.contractdrivendevelopmentsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractDrivenDevelopmentSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContractDrivenDevelopmentSampleApplication.class, args);
+	}
+}
