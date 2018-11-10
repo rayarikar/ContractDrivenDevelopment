a1d527895930b335a8aa905348808dad3910145a
Rohan Rayarikar <rayarikar.rohan@gmail.com>
2018-11-10 17:09:56 -0500

    Sample hello world resource

--- src/main/java/com/rohan/development/contractdrivendevelopmentsample/endpoints/HelloWorldResource.java
+++ src/main/java/com/rohan/development/contractdrivendevelopmentsample/endpoints/HelloWorldResource.java
@@ -1,7 +1,13 @@
 package com.rohan.development.contractdrivendevelopmentsample.endpoints;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldResource {
+
+    @RequestMapping("/")
+    public String helloWorld() {
+        return "Hello world from SpringBoot";
+    }
 }
