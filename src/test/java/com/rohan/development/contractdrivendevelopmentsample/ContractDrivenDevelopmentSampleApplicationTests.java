0c6598cd66ac82f8636050a561befb486dc8e4a2
Rohan Rayarikar <rayarikar.rohan@gmail.com>
2018-12-15 20:13:34 -0500

    Adding annotation to delete the test context

--- src/test/java/com/rohan/development/contractdrivendevelopmentsample/ContractDrivenDevelopmentSampleApplicationTests.java
+++ src/test/java/com/rohan/development/contractdrivendevelopmentsample/ContractDrivenDevelopmentSampleApplicationTests.java
@@ -3,10 +3,12 @@ package com.rohan.development.contractdrivendevelopmentsample;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@DirtiesContext
 public class ContractDrivenDevelopmentSampleApplicationTests {
 
 	@Test
